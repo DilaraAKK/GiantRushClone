8042a62f20383121ce68a356a388518165afedd0
DilaraAK <dilaraakpiinar@gmail.com>
2022-04-09 15:24:45 +0300

    Camera Follow Script added and some offset changes manually in Inspector.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public float smoothing = 5f;
+    public Vector3 offset;
+    public Vector3 angleOffset;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.position + offset, smoothing * Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(player.eulerAngles + angleOffset), Time.deltaTime * 5);
+
+    }
+}
